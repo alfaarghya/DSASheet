3aee73bd84d772eac80ebced62f50a56e61dec4e
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2023-01-13 00:20:47 +0530

    Update Array05_ChocolateDistributionProblem.java

--- Array05_ChocolateDistributionProblem.java
+++ Array05_ChocolateDistributionProblem.java
@@ -60,9 +60,8 @@ public class Array05_ChocolateDistributionProblem {
         int arr1[] = {7, 3, 2, 4, 9, 12, 56};
         int arr2[] = {3, 4, 1, 9, 56, 7, 9, 12};
         int arr3[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
+        int arr_worstCase[] = {32,24,15,46,52,57,5,39};
         
-        System.out.println("Minimum difference -> "+minDiff(arr2, 5));
-        
-        
+        System.out.println("Minimum difference -> "+minDiff(arr_worstCase, 2));
     }
 }
