0c85537415771b1c9006b2de52de65bb0e01fab6
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2023-03-17 10:34:42 +0530

    Update BST03_ValidBST.java

--- Binary Search Tree/BST03_ValidBST.java
+++ Binary Search Tree/BST03_ValidBST.java
@@ -13,20 +13,35 @@ public class BST03_ValidBST {
         }
     }
     /*--- ----*/
+
+    /*----- Approach 1 -> check the tree for it's validation -----*/
          /*---- In-Order Traversal ----*/
-    public static void inOrderTraversal(Node root){
+    public static void inOrderTraversal(Node root, ArrayList<Integer> list){
             //base case
         if(root == null){
             return; 
         }
 
-        inOrderTraversal(root.left);    //check left side of current root
-        System.out.print(root.data+" ");
-        inOrderTraversal(root.right);   //check right side of current root
+        inOrderTraversal(root.left, list);    //check left side of current root
+        // System.out.print(root.data+" ");
+        list.add(root.data);
+        inOrderTraversal(root.right, list);   //check right side of current root
     }
                 /*---- ----*/
 
-                /*---- check the tree for it's validation ----*/
+    public static boolean isBSTValid(Node root, ArrayList<Integer> list){   //TC -> O(n) SC -> O(n  )
+        inOrderTraversal(root, list);   //call this to get the sorted from of BSt
+
+        for(int i = 0; i < list.size()-1; i++){
+            if(list.get(i) > list.get(i+1)){    //when current data is greater than it's next data
+                return false;   //......that's means it is not a valid BST
+            }
+        }
+
+        return true;    //if loop end that's mean BST is valid
+    }
+    /*-------- --------*/
+        /*----Approach 2 -> check the tree for it's validation ----*/
     public static boolean isBSTValid(Node root, Node min, Node max){    //TC -> O(n)
             //base case
         if(root == null){
@@ -45,6 +60,11 @@ public class BST03_ValidBST {
     }
                                 /*---- ----*/
 
+    public static void printArrayList(ArrayList<Integer> list){
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
     public static void main(String[] args) {
         /*
                         8
@@ -58,15 +78,26 @@ public class BST03_ValidBST {
         Node root = new Node(8);
         root.left = new Node(5);
         root.left.left = new Node(3);
-        root.left.left.left = new Node(1);  //change it's value and recheck........greater than 8
-        root.left.left.right = new Node(4);
+        root.left.left.left = new Node(1);  //for Approach 2 -> change it's value and recheck........greater than 8
+        root.left.left.right = new Node(4);//for Approach 1 -> change it's value less than it's previous value
         root.left.right = new Node(6);
         root.right = new Node(10);
-        root.right.right = new Node(11);
-        root.right.right.right = new Node(14);  //change it's value and recheck........ less than 8
+        root.right.right = new Node(11);//for Approach 1 -> change it's value greater than it's next value
+        root.right.right.right = new Node(14);  //for Approach 2 -> change it's value and recheck........ less than 8
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+            //Approach 1
+        /*boolean ans = isBSTValid(root, list);
         System.out.print("Original BST >> ");
-        inOrderTraversal(root);
+        printArrayList(list);
+        System.out.println();
+        System.out.println("is BST valid?(true/false) \n>> "+ans);*/
+
+            //Approach 2
+        System.out.print("Original BST >> ");
+        inOrderTraversal(root, list);
+        printArrayList(list);
         System.out.println();
         System.out.println("is BST valid?(true/false) \n>> "+isBSTValid(root, null, null));
     }
