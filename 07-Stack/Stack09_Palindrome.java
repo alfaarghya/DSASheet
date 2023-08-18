d7d0de316a1ca6d76afd390b04ef70e278d17202
alfaArghya <arghya.coding@gmail.com>
2023-08-18 07:21:25 +0530

    testing branches

--- 07-Stack/Stack09_Palindrome.java
+++ 07-Stack/Stack09_Palindrome.java
@@ -1,12 +1,8 @@
 import java.util.LinkedList;
 
-import StackWithLinkedList.Node;
 
 public class Stack09_Palindrome {
 
-    public static boolean isPalindrome(LinkedList<Character> ll) {
-        Node head = ll.getFirst();   
-    }
 
     public static void main(String[] args) {
         LinkedList<Character> ll = new LinkedList<>();
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 I try to solve DSA questions and this repository contains java codes only.
 
 currently I am learning git.
+
+testing branches
