cdd71112f224e4dc63907c4294df91eeef9c3634
alfaArghya <arghya.coding@gmail.com>
2023-10-02 23:35:48 +0530

    start learning Topological Sorting

---
+++ 15-Graph/Graph04TopologicalSorting.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Edge {
+
+    int source;
+    int destination;
+    // int weight;
+
+    public Edge(int source, int destination) {
+        this.source = source;
+        this.destination = destination;
+        // this.weight = weight;
+    }
+}
+
+public class Graph04TopologicalSorting {
+    public static void main(String[] args) {
+
+    }
+}
