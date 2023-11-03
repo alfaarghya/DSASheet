f54220f752d00043942f852f60038a1e9875313b
alfaArghya <arghya.coding@gmail.com>
2023-11-03 12:54:19 +0530

    addinig come comment

--- 16-DynamicProgramming/DP01_Fibonacci.java
+++ 16-DynamicProgramming/DP01_Fibonacci.java
@@ -1,6 +1,7 @@
 
 public class DP01_Fibonacci {
 
+    // recursion
     public static int fib(int n) {
         if (n == 0 || n == 1)
             return n;
@@ -8,28 +9,34 @@ public class DP01_Fibonacci {
         return fib(n - 1) + fib(n - 2);
     }
 
+    // DP-memoization
     public static int fibMemoization(int n, int[] dp) {
         if (n == 0 || n == 1)
             return n;
 
         if (dp[n] != 0)
             return dp[n];
+
         dp[n] = fibMemoization(n - 1, dp) + fibMemoization(n - 2, dp);
         return dp[n];
     }
 
+    // DP-tabulation
     public static int fibTabulation(int n) {
         if (n == 0 || n == 1)
             return n;
 
+        // initialize
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
 
+        // loop
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
+        // get the ans
         return dp[n];
     }
 
