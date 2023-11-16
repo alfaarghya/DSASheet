5a4f89b0b5dfa928b6927a24cdad498c18421b32
alfaArghya <arghya.coding@gmail.com>
2023-11-16 09:24:08 +0530

    solved using Tabulation

--- 16-DynamicProgramming/DP03_01Knapsack.java
+++ 16-DynamicProgramming/DP03_01Knapsack.java
@@ -50,7 +50,57 @@ public class DP03_01Knapsack {
             return dp[n][wt];
         }
     }
+
     /* ---- ---- */
+    /* ---- tabulation method ---- || TC -> O(n * wt) */
+    public static int knapsackTabulation(int[] val, int[] w, int wt) {
+        // 1. initialization
+        int[][] dp = new int[val.length + 1][wt + 1];
+
+        // 2. filling
+        // base case --> n = 0 -> 0 || wt = 0 -> 0
+        for (int i = 0; i < dp.length; i++) { // 0th column
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {// 0th row
+            dp[0][j] = 0;
+        }
+
+        // i -- refers to --> values or weights
+        for (int i = 1; i < dp.length; i++) {
+            // j -- refers to --> weight of knapsack
+            for (int j = 1; j < dp[0].length; j++) {
+                // i-1th value & weight
+                int valTemp = val[i - 1];
+                int wTemp = w[i - 1];
+
+                if (wTemp <= j) {
+                    int include = valTemp + dp[i - 1][j - wTemp];
+                    int exclude = dp[i - 1][j];
+                    dp[i][j] = Math.max(include, exclude);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        printArray(dp);
+
+        // return last cell
+        return dp[val.length][wt];
+
+    }
+
+    /* ---- ---- */
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         int[] value = { 15, 14, 10, 45, 30 };
@@ -58,6 +108,7 @@ public class DP03_01Knapsack {
         int wt = 7;
 
         System.out.println(knapsack(value, weight, wt, value.length));
+        System.out.println("---------------------------------------------------------");
         // -----------------------------------------
         int[][] dp = new int[value.length + 1][wt + 1];
         for (int i = 0; i < dp.length; i++) {
@@ -67,5 +118,9 @@ public class DP03_01Knapsack {
         }
 
         System.out.println(knapsackMemoization(value, weight, dp, wt, value.length));
+        printArray(dp);
+        System.out.println("---------------------------------------------------------");
+        System.out.println(knapsackTabulation(value, weight, wt));
+        System.out.println("---------------------------------------------------------");
     }
 }
