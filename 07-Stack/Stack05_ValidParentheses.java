2643189fd12948b47f0a08118194d376cf3f8851
alfaArghya <arghya.coding@gmail.com>
2024-02-11 21:56:17 +0530

    solved LC20

--- 07-Stack/Stack05_ValidParentheses.java
+++ 07-Stack/Stack05_ValidParentheses.java
@@ -1,3 +1,31 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */
+
+
 import java.util.Stack;
 
 public class Stack05_ValidParentheses {
@@ -11,8 +39,11 @@ public class Stack05_ValidParentheses {
             if(ch == '[' || ch == '{' || ch == '(') {
                 stk.push(ch);
             } else {
+                if(stk.isEmpty()) {
+                    return false;
+                }
                     //brackets pair -> pop from stk
-                if( (stk.peek() == '[' && ch == ']') || (stk.peek() == '{' && ch == '}') || (stk.peek() == '(' && ch == ')')) {
+                else if( (stk.peek() == '[' && ch == ']') || (stk.peek() == '{' && ch == '}') || (stk.peek() == '(' && ch == ')')) {
                     stk.pop();
                 } else {    //dose not make a pair
                     return false;
@@ -32,6 +63,9 @@ public class Stack05_ValidParentheses {
         String str1 = "[]{([])[]}";
         String str2 = "[{()()(}]}";
         String str3 = "(}";
-        System.out.println(isValidParentheses(str3));
+        String str4 = "()";
+        String str5 = "(){}[]";
+        String str6 = "]";
+        System.out.println(isValidParentheses(str6));
     }
 }
