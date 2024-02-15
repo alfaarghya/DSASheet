197fa69472d1bbb13e40fc5ec7ad3d23cbd40eae
alfaArghya <arghya.coding@gmail.com>
2024-02-15 20:44:55 +0530

    solved LC2971

--- 09-GreedyAlgorithum/Greedy08_FindPolygonWithLargestPerimeter.java
+++ 09-GreedyAlgorithum/Greedy08_FindPolygonWithLargestPerimeter.java
@@ -31,37 +31,38 @@ Constraints:
 1 <= nums[i] <= 109
  */
 
-import java.util.Arrays;
+ import java.util.Arrays;
 
-public class Array47_FindPolygonWithLargestPerimeter {
-    
-    public static long largestPerimeter(int[] nums) {
-        if(nums.length < 3) {
-            return -1;
-        }
-
-        Arrays.sort(nums);
-
-        long ans = -1;
-        long prevSum = 0;
-
-        for(int num: nums) {
-            
-            if(num < prevSum) {
-                ans = num + prevSum;
-            }
-
-            prevSum += num;
-
-        }
-
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        int[] arr1 = {5,5,5};
-        int[] arr2 = {1,12,1,2,5,50,3};
-
-        System.out.println(largestPerimeter(arr2));
-    }
-}
+ public class Greedy08_FindPolygonWithLargestPerimeter {
+     
+     public static long largestPerimeter(int[] nums) {
+         if(nums.length < 3) {
+             return -1;
+         }
+ 
+         Arrays.sort(nums);
+ 
+         long ans = -1;
+         long prevSum = 0;
+ 
+         for(int num: nums) {
+             
+             if(num < prevSum) {
+                 ans = num + prevSum;
+             }
+ 
+             prevSum += num;
+ 
+         }
+ 
+         return ans;
+     }
+ 
+     public static void main(String[] args) {
+         int[] arr1 = {5,5,5};
+         int[] arr2 = {1,12,1,2,5,50,3};
+ 
+         System.out.println(largestPerimeter(arr2));
+     }
+ }
+ 
